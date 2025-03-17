f87b8be8b45ea15ca9aa8ce7e8dd6e93159031b1
laote <ihagiu1212@gmail.com>
2025-03-17 14:20:27 +0200

    creating the main functin with a while  loop and added the functionality of the app, now is time to start implementing the functionality for each user choice

--- main.py
+++ main.py
@@ -43,3 +43,37 @@ def delete_tasks():
 
 def mark_task_completion():
     pass
+
+
+
+# here we gonna handle all the function that was created above
+def main():
+    tasks = load_task()
+    while True:
+        print("\n To-Do List Application")
+        print("1. Add Tasks")
+        print("2. View Tasks")
+        print("3. Delete Tasks")
+        print("4. Mark Tasks as completed")
+        print("5. Exit")
+
+        choice = input("Enter you choice: ").strip()
+        if choice == 1:
+            add_tasks()
+        elif choice == 2:
+            view_tasks()
+        elif choice == 3:
+            delete_tasks()
+        elif choice == 4:
+            mark_task_completion()
+        elif choice == 5:
+            print("Exit the program")
+            break
+        else:
+            print("Pick a valid choice")
+
+
+
+
+# Call the main function
+main()
